b9cc973f127f87f0b1887f4d4f5522e1810f96ea
nayoonchoi <nayoon210@naver.com>
2024-06-16 16:45:18 +0900

    first commit

---
+++ js/mail_old.js
@@ -0,0 +1,46 @@
+// import { createTransport } from 'nodemailer';
+//let nodemailer = require("nodemailer");
+import * as nodemailer from 'nodemailer';
+let senderInfo = require('../config/senderInfo.json');
+//import senderInfo from '../config/senderInfo.json';
+// 메일발송 객체
+
+  // 메일발송 함수
+
+  export function sendGmail (param) {
+    console.log(param+"가져옴");
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',   // 메일 보내는 곳
+      prot: 587,
+      host: 'smtp.gmail.com',  
+      secure: false,  
+      requireTLS: true ,
+      auth: {
+        user: senderInfo.user,  // 보내는 메일의 주소
+        pass: senderInfo.pass   // 보내는 메일의 비밀번호
+      }
+    });
+    // 메일 옵션
+    let mailOptions = {
+      from: senderInfo.user, // 보내는 메일의 주소
+      to:senderInfo.user, // 수신할 이메일
+      subject: "내쓰플 문의입니다", // 메일 제목
+      text: param // 메일 내용
+    };
+    console.log(mailOptions);
+    // 메일 발송    
+    transporter.sendMail(mailOptions, function (error, info) {
+      console.log("sendmail 실행됨");
+      console.log(mailOptions);
+      if (error) {
+        return(error);
+      } else {
+        return('Email sent: ' + info.response);
+      }
+    });
+
+  }
+
+
+
+//module.exports = mailSender;
